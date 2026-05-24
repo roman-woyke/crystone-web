d49b73c0ab9a4ea2625b9c735a0d2ac93fe81db2
Roman Woyke <roman.woyke@hm.edu>
2026-05-24 13:05:31 +0200

    footer buttons to switch branches

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,13 @@
 </main>
 
+<footer class="site-footer">
+    <span class="footer-label">Switch to:</span>
+    <?php
+        $page = basename($_SERVER['SCRIPT_NAME']);
+    ?>
+    <a href="/basti/<?= $page ?>">Basti</a>
+    <a href="/ben/<?= $page ?>">Ben</a>
+</footer>
+
 </body>
 </html>
